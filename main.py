a11f749ebfe3a5faa0948ae66d3a6ddfe0f1090e
Designerkt <doanhandsome23@gmail.com>
2023-01-15 12:21:18 +0700

    test

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ dimensions = src.shape
 # height, width, number of channels in image
 height = src.shape[0]
 width = src.shape[1]
-mask_y = src.shape[0]/15
+mask_y = int(height / 15)
 
 # Crop image
 y = 0
@@ -35,27 +35,39 @@ h = height - mask_y
 w = width
 crop = dst[y:y + h, x:x + w]
 
+# Resize image
+r_src = cv.resize(src, (320, 240))
+r_dst = cv.resize(dst, (320, 240))
+r_crop = cv.resize(crop, (320, 224))
+
 # Histogram calculation
 hist = np.histogram(crop, 256, [0, 256])
 plt.hist(crop.ravel(), 256, [0, 256])
 
+# Color calculation
+white = 0
+black = 0
+for i in range(20):
+    white += hist[0][i]
+for i in range(225, 250):
+    black += hist[0][i]
+
 # Display results
-cv.imshow('Source image', src)
-cv.imshow('Equalized Image', dst)
-cv.imshow('Crop Image', crop)
+def showInMovedWindow(winname, img, x, y):
+    cv.namedWindow(winname)  # Create a named window
+    cv.moveWindow(winname, x, y)  # Move it to (x,y)
+    cv.imshow(winname, img)
+showInMovedWindow('Source image', r_src, 0, 200)
+cv.imshow('Source image', r_src)
+cv.imshow('Equalized Image', r_dst)
+cv.imshow('Crop Image', r_crop)
 
 plt.title('Histogram for gray scale picture')
 plt.show()
 
-print(hist[0])
-
-sum_white = 0
-sum_black = 0
-for i in range(20):
-    sum_white += hist[0][i]
-for i in range(225, 250):
-    sum_black += hist[0][i]
-print(sum_white)
-print((sum_white / sum_black) * 100, " %")
+# print(hist[0])
+print(white)
+print(black)
+print(round((white / black) * 100, 2), " %")
 # Wait until user exits the program
 cv.waitKey()
