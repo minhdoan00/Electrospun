911246acfb7878cc5054df392c82c3e7aee349b4
Designerkt <doanhandsome23@gmail.com>
2023-01-15 09:52:50 +0700

    test

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from matplotlib import pyplot as plt
 
 # Load image
 parser = argparse.ArgumentParser(description='Code for Histogram Equalization tutorial.')
-parser.add_argument('--input', help='Path to input image.', default='C:/Users/MINHDOAN/Downloads/Pictures/DieuAnh.jpg')
+parser.add_argument('--input', help='Path to input image.', default='image/Si-PVA_q004.tif')
 args = parser.parse_args()
 
 src = cv.imread(cv.samples.findFile(args.input))
@@ -26,12 +26,12 @@ dimensions = src.shape
 # height, width, number of channels in image
 height = src.shape[0]
 width = src.shape[1]
-mask = 200
+mask_y = src.shape[0]/15
 
 # Crop image
 y = 0
 x = 0
-h = height - mask
+h = height - mask_y
 w = width
 crop = dst[y:y + h, x:x + w]
 
@@ -39,21 +39,23 @@ crop = dst[y:y + h, x:x + w]
 hist = np.histogram(crop, 256, [0, 256])
 plt.hist(crop.ravel(), 256, [0, 256])
 
-
 # Display results
-# cv.imshow('Source image', src)
-# cv.imshow('Equalized Image', dst)
-# cv.imshow('Crop Image', crop)
-#
-# plt.title('Histogram for gray scale picture')
-# plt.show()
+cv.imshow('Source image', src)
+cv.imshow('Equalized Image', dst)
+cv.imshow('Crop Image', crop)
 
-print(hist)
+plt.title('Histogram for gray scale picture')
+plt.show()
 
+print(hist[0])
+
+sum_white = 0
 sum_black = 0
-for i in range(256):
+for i in range(20):
+    sum_white += hist[0][i]
+for i in range(225, 250):
     sum_black += hist[0][i]
-print(sum_black)
+print(sum_white)
+print((sum_white / sum_black) * 100, " %")
 # Wait until user exits the program
 cv.waitKey()
-
