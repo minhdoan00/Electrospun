3539e5ce4dc99238969f466da2ee40e16a4185da
Designerkt <doanhandsome23@gmail.com>
2023-01-15 13:43:14 +0700

    test

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from matplotlib import pyplot as plt
 
 # Load image
 parser = argparse.ArgumentParser(description='Code for Histogram Equalization tutorial.')
-parser.add_argument('--input', help='Path to input image.', default='image/Si-PVA_q004.tif')
+parser.add_argument('--input', help='Path to input image.', default='image/PVAZnO_q006.tif')
 args = parser.parse_args()
 
 src = cv.imread(cv.samples.findFile(args.input))
@@ -47,27 +47,33 @@ plt.hist(crop.ravel(), 256, [0, 256])
 # Color calculation
 white = 0
 black = 0
-for i in range(20):
+for i in range(0, 29):
     white += hist[0][i]
-for i in range(225, 250):
+for i in range(220, 256):
     black += hist[0][i]
 
+
 # Display results
+# cv.imshow('Source image', r_src)
+# cv.imshow('Equalized Image', r_dst)
+# cv.imshow('Crop Image', r_crop)
 def showInMovedWindow(winname, img, x, y):
     cv.namedWindow(winname)  # Create a named window
     cv.moveWindow(winname, x, y)  # Move it to (x,y)
     cv.imshow(winname, img)
-showInMovedWindow('Source image', r_src, 0, 200)
-cv.imshow('Source image', r_src)
-cv.imshow('Equalized Image', r_dst)
-cv.imshow('Crop Image', r_crop)
 
-plt.title('Histogram for gray scale picture')
-plt.show()
 
-# print(hist[0])
+# showInMovedWindow('Source image', r_src, 50, 200)
+# showInMovedWindow('Equalized Image', r_dst, 400, 200)
+# showInMovedWindow('Crop Image', r_crop, 800, 205)
+#
+# plt.title('Histogram for gray scale picture')
+# plt.show()
+
+print(type(crop))
 print(white)
 print(black)
-print(round((white / black) * 100, 2), " %")
+# print(round((white / black) * 100, 3), " %")
+print(round((white / black) * 100, 3), " %")
 # Wait until user exits the program
 cv.waitKey()
