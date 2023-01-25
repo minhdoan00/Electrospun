0e056ad1ce0da1aab007282425f2ab51e4c6cd20
Designerkt <doanhandsome23@gmail.com>
2023-01-25 20:09:29 +0700

    test

--- line_detect.py
+++ line_detect.py
@@ -1,35 +1,36 @@
-from __future__ import print_function
 import cv2
-import argparse
 import numpy as np
-from matplotlib import pyplot as plt
-img = cv2.imread('image/PVAZnO_q001.tif')
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 
-kernel_size = 5
-blur_gray = cv2.GaussianBlur(gray,(kernel_size, kernel_size),0)
+# Read image
+image = cv2.imread('image/PVAZnO_q004.tif')
 
-low_threshold = 50
-high_threshold = 150
-edges = cv2.Canny(blur_gray, low_threshold, high_threshold)
+# Convert image to grayscale
+gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
 
-rho = 1  # distance resolution in pixels of the Hough grid
-theta = np.pi / 180  # angular resolution in radians of the Hough grid
-threshold = 15  # minimum number of votes (intersections in Hough grid cell)
-min_line_length = 50  # minimum number of pixels making up a line
-max_line_gap = 20  # maximum gap in pixels between connectable line segments
-line_image = np.copy(img) * 0  # creating a blank to draw lines on
+# Use canny edge detection
+edges = cv2.Canny(gray, 50, 150, apertureSize=3)
 
-# Run Hough on edge detected image
-# Output "lines" is an array containing endpoints of detected line segments
-lines = cv2.HoughLinesP(edges, rho, theta, threshold, np.array([]),
-                    min_line_length, max_line_gap)
+# Apply HoughLinesP method to
+# to directly obtain line end points
+lines_list = []
+lines = cv2.HoughLinesP(
+    edges,  # Input edge image
+    1,  # Distance resolution in pixels
+    np.pi / 180,  # Angle resolution in radians
+    threshold=100,  # Min number of votes for valid line
+    minLineLength=5,  # Min allowed length of line
+    maxLineGap=10  # Max allowed gap between line for joining them
+)
 
-for line in lines:
-    for x1,y1,x2,y2 in line:
-        cv2.line(line_image,(x1,y1),(x2,y2),(255,0,0),5)
+# Iterate over points
+for points in lines:
+    # Extracted points nested in the list
+    x1, y1, x2, y2 = points[0]
+    # Draw the lines joing the points
+    # On the original image
+    cv2.line(image, (x1, y1), (x2, y2), (0, 255, 0), 2)
+    # Maintain a simples lookup list for points
+    lines_list.append([(x1, y1), (x2, y2)])
 
-# Draw the lines on the  image
-lines_edges = cv2.addWeighted(img, 0.8, line_image, 1, 0)
-
-cv2.imshow("line detect", img)
+# Save the result image
+cv2.imwrite('detectedLines.png', image)
